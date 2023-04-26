7cfba2996919a518b31fa1c03c89b913afd55f73
Elise Deshusses <ejdeshusses@wpi.edu>
2023-04-25 21:45:20 -0400

    Adding ranking formulas in a new file

--- gradio_gui.py
+++ gradio_gui.py
@@ -1,13 +1,13 @@
-from cosine_similarity_baseline import generate_top_ten
+from general_cosine_sim_base import generate_top_ten
 import gradio as gr
 
 
 def gradio_gui():
     
     with gr.Blocks() as demo:
-        keywords = gr.Textbox(label="Keywords (seperate with space)")
-        semantics = gr.Radio(['both', 'pos', 'neg'], label="Do you want to see positive or negative review? (default is both)")
-        years = gr.CheckboxGroup(["2000", "2001", "2002","2003", "2004", "2005"], label="Year", info="What year of review?")
+        keywords = gr.Textbox(label="Keywords (Seperate with a space. Do not leave a space after the last word)")
+        semantics = gr.Radio(['both', 'pos', 'neg'], label="Do you want to see recommendations from only positive or negative reviews? (default is both)")
+        years = gr.CheckboxGroup(["2000", "2001", "2002","2003", "2004", "2005"], label="Year", info="Recommendations based on specific review year(s)?")
         outputs = gr.Dataframe(row_count = (10, "dynamic"), col_count=(2, "dynamic"), label="Generated List")
         with gr.Tab("Baseline Cosine Similarity"):
             generate_btn = gr.Button("generate")
