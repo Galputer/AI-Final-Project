7ec42b201ad7c9cc151fe014dac37847081d95c1
Shun Li <sli19@wpi.edu>
2023-05-03 03:05:27 -0400

    merge emily's apporaches with keywords matching

--- gradio_gui.py
+++ gradio_gui.py
@@ -2,6 +2,8 @@ from cosine_similarity_baseline import generate_top_ten
 from cosine_similarity_keyword import generate_top_ten_v2
 import gradio as gr
 
+from cosine_similarity_merged import generate_top_ten_v3, generate_top_ten_v4
+
 
 def gradio_gui():
     
@@ -10,14 +12,22 @@ def gradio_gui():
         semantics = gr.Radio(['both', 'pos', 'neg'], label="Do you want to see positive or negative review? (default is both)")
         years = gr.CheckboxGroup(["2000","2001", "2002","2003", "2004", "2005"], label="Year", info="What year of review?")
         outputs = gr.Dataframe(row_count = (10, "dynamic"), col_count=(2, "dynamic"), label="Generated List")
+        with gr.Tab("Keyword Curated List"):
+            generate_btn = gr.Button("generate")
+            generate_btn.click(fn = generate_top_ten_v4, inputs = [keywords,semantics,years], outputs = outputs)
+        with gr.Tab("Baseline Curated List"):
+            generate_btn = gr.Button("generate")
+            generate_btn.click(fn = generate_top_ten_v3, inputs = [keywords,semantics,years], outputs = outputs)
+        with gr.Tab("Keyword Cosine Similarity"):
+            generate_btn = gr.Button("generate")
+            generate_btn.click(fn = generate_top_ten_v2, inputs = [keywords,semantics,years], outputs = outputs)
         with gr.Tab("Baseline Cosine Similarity"):
             generate_btn = gr.Button("generate")
             generate_btn.click(fn = generate_top_ten, inputs = [keywords,semantics,years], outputs = outputs)
-        with gr.Tab("Updated model"):
-            generate_btn = gr.Button("generate")
-            generate_btn.click(fn = generate_top_ten_v2, inputs = [keywords,semantics,years], outputs = outputs)
+
     demo.queue()
-    demo.launch(share=True)
+    # demo.launch(share=True)
+    demo.launch()
     
     
 if __name__ == "__main__":
--- year-split.py
+++ year-split.py
@@ -3,10 +3,11 @@ from tqdm import tqdm
 
 df = pd.read_csv('archive/amazon_reviews_us_Books_v1_02.tsv', sep='\t', error_bad_lines=False)
 df = df.dropna()
+
 # Combine review headline and review body into a single column
 df['clean_text'] = df['review_headline'] + ' ' + df['review_body']
-df = df[['product_id', 'product_title','review_date', 'star_rating', 'helpful_votes', 
-          'total_votes', 'vine', 'verified_purchase', 'clean_text']]
+df = df[['product_id', 'product_title','product_parent','review_date', 'star_rating', 'helpful_votes', 
+          'total_votes', 'vine', 'verified_purchase', 'review_headline','review_body','clean_text']]
 mask0 = (df['review_date'] >= '2000-1-1') & (df['review_date'] <= '2000-12-31')
 mask1 = (df['review_date'] >= '2001-1-1') & (df['review_date'] <= '2001-12-31')
 mask2 = (df['review_date'] >= '2002-1-1') & (df['review_date'] <= '2002-12-31')
@@ -18,5 +19,5 @@ masks = [mask0,mask1,mask2,mask3,mask4,mask5]
 year = 2000
 for the_mask in tqdm(masks):
     cur_df = df.loc[the_mask]
-    cur_df.to_csv(("book-csv/"+str(year)+".csv"), index = None)
+    cur_df.to_csv(("book-csv-2/"+str(year)+".csv"), index = None)
     year += 1
