d9273323a45bd9ed504be69b72a3f58f99245655
Shun Li <sli19@wpi.edu>
2023-05-03 18:01:33 -0400

    Add Daniel's work to the demo

--- cosine_similarity_merged.py
+++ cosine_similarity_merged.py
@@ -5,6 +5,8 @@ from sklearn.metrics.pairwise import cosine_similarity
 import pandas as pd
 from tqdm import tqdm
 
+from functions import get_price_from_list_IDs
+
 # For the Formula : 
 HELPFUL_VOTES_MULTIPLIER = 0.001
 TOTAL_VOTES_MULTIPLIER = 0.0005
@@ -31,7 +33,7 @@ def filter_pickle(r_df, search_words):
 
     return df
 
-def generate_top_ten_v3(keywords = 'math science book text', semantic = 'pos', filename_list = ['2005','2004','2003','2002','2001','2000']):
+def generate_top_ten_v3(keywords = 'math science book text', semantic = 'pos', filename_list = ['2005']):
 
     # Load pre-trained BERT model and tokenizer
     MODEL = 'bert-base-uncased'
@@ -151,7 +153,7 @@ def generate_top_ten_v3(keywords = 'math science book text', semantic = 'pos', f
     top_10.to_csv('top10_output.csv')
     return(top_10[['product_title','rec-score']])
 
-def generate_top_ten_v4(keywords = 'math science book text', semantic = 'pos', filename_list = ['2005','2004','2003','2002','2001','2000']):
+def generate_top_ten_v4(keywords = 'math science book text', semantic = 'pos', filename_list = ['2005']):
 
     # Load pre-trained BERT model and tokenizer
     MODEL = 'bert-base-uncased'
@@ -164,12 +166,8 @@ def generate_top_ten_v4(keywords = 'math science book text', semantic = 'pos', f
 
     # Encode keyword and text tokens using BERT
     encoded_keywords = tokenizer(keywords.lower(), return_tensors='pt')['input_ids']
-    # encoded_texts = tokenizer(list(df['clean_text']), padding=True, truncation=True, return_tensors='pt')['input_ids']
 
-    # filename_list = ['2005','2004','2003','2002','2001','2000']
-    filename_list = ['2005']
     df_lst = []
-    ## Headers: ['product_id', 'product_title', 'review_date', 'star_rating', 'helpful_votes', 'total_votes', 'vine', 'verified_purchase', 'clean_text', 'sentiment-score', 'embeddings']
 
     # Generate BERT embeddings for keyword and text tokens batch-wise and compute cosine similarity
     with torch.no_grad():
@@ -180,13 +178,6 @@ def generate_top_ten_v4(keywords = 'math science book text', semantic = 'pos', f
             df = pd.read_pickle('book-pickle-4/'+ filename +'.pkl')
         except(FileNotFoundError):
             continue
-        # print(df['embeddings'].head(10))
-        # break
-
-        # print("List of DF headers ", list(df.columns.values))
-
-        # batch_size = 64
-        # total_batches = (len(df) + batch_size - 1) // batch_size
 
         df['similarity_score'] = np.nan 
         
@@ -201,11 +192,6 @@ def generate_top_ten_v4(keywords = 'math science book text', semantic = 'pos', f
         df_lst.append(df)
 
     big_df = pd.concat(df_lst)
-    # print("   Verified ", big_df['verified_purchase'].head(10))
-    # print("   helpful votes ", big_df['helpful_votes'].head(10))
-    # print("   total votes ", big_df['total_votes'].head(10))
-    # print("   sentiment ", big_df['sentiment-score'].head(10))
-    # print("   similarity ", big_df['similarity_score'].head(10))
 
     # Make new copy dataframe for manipulation
     recommended = big_df[['product_id','product_title','helpful_votes', 'total_votes', 'sentiment-score']].copy()
@@ -268,5 +254,114 @@ def generate_top_ten_v4(keywords = 'math science book text', semantic = 'pos', f
     top_10.to_csv('top10_output.csv')
     return(top_10[['product_title','rec-score']])
 
+def generate_top_ten_with_prices(keywords = 'math science book text', semantic = 'pos', filename_list = ['2005']):
+    # Load pre-trained BERT model and tokenizer
+    MODEL = 'bert-base-uncased'
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    tokenizer = AutoTokenizer.from_pretrained(MODEL)
+    model = AutoModel.from_pretrained(MODEL).to(device)
+    model.save_pretrained(MODEL)
+    tokenizer.save_pretrained(MODEL)
+
+    # Encode keyword and text tokens using BERT
+    encoded_keywords = tokenizer(keywords.lower(), return_tensors='pt')['input_ids']
+
+    df_lst = []
+
+    # Generate BERT embeddings for keyword and text tokens batch-wise and compute cosine similarity
+    with torch.no_grad():
+        keyword_embeddings = model(encoded_keywords.to(device))[0][:, 0, :].cpu().numpy()
+    for filename in filename_list:
+        # Load DataFrame
+        try:
+            df = pd.read_pickle('book-pickle-4/'+ filename +'.pkl')
+        except(FileNotFoundError):
+            continue
+
+        df['similarity_score'] = np.nan 
+        
+        for idx in tqdm(range(len(df))):
+
+            text_embeddings = df.iloc[idx]['keywords_embedding']
+            # Compute cosine similarity between keyword vector and text vectors in the batch
+            try:
+                max_similarity_score = max(cosine_similarity(keyword_embeddings, text_embeddings)[0])
+            except:
+                continue
+            # Add similarity scores to DataFrame
+            df.at[idx, 'similarity_score'] = max_similarity_score
+
+        df_lst.append(df)
+
+    big_df = pd.concat(df_lst)
+
+    # Make new copy dataframe for manipulation
+    recommended = big_df[['product_id','product_title','helpful_votes', 'total_votes', 'sentiment-score']].copy()
+    # recommended = big_df[['product_id','product_title','star_rating','helpful_votes', 'total_votes', 'sentiment-score']].copy()
+    print(big_df['similarity_score'].head(10))
+
+    recommended['similarity_score'] =  big_df['similarity_score'].astype('float64')
+    
+    # print("  INFO \n", recommended.info())
+    
+    # filter semantic flag
+    if semantic == 'neg':
+        recommended = recommended[recommended['sentiment-score'] < -SEMANTIC_THRESHOLD]
+    elif semantic == 'pos':
+        recommended = recommended[recommended['sentiment-score'] > SEMANTIC_THRESHOLD]
+    
+    # filter semantic flag
+    if semantic == 'neg':
+        recommended = recommended[recommended['similarity_score'] < -SEMANTIC_THRESHOLD]
+    elif semantic == 'pos':
+        recommended = recommended[recommended['similarity_score'] > SEMANTIC_THRESHOLD]
+    
+
+    if FORMULA == 1: 
+        recommended['weight-score'] = ( ((recommended['helpful_votes'].mul(HELPFUL_VOTES_MULTIPLIER)).add(recommended['total_votes'].mul(TOTAL_VOTES_MULTIPLIER))).add(1) )
+        recommended['rank-score'] = ( (recommended['weight-score']).mul(recommended['sentiment-score']))
+        recommended['rec-score'] = ( (recommended['rank-score']).add((recommended['similarity_score'].mul(SIMILARITY_MULTIPLIER))))
+        # print("   calculated : ", recommended.head(10))
+
+        grouped_df = recommended.groupby(['product_id', 'product_title'], as_index=False).mean()
+
+        print("   GROUPED : \n", grouped_df.head(10))
+    
+    elif FORMULA == 2: 
+        recommended['weight-score'] = ( ((recommended['helpful_votes'].add(recommended['total_votes'])).mul(ALL_VOTES_MULTIPLIER)).add(1) )
+        recommended['rank-score'] = ( (recommended['weight-score']).mul(recommended['sentiment-score']))
+        recommended['rec-score'] = ( (recommended['rank-score']).add((recommended['similarity_score'].mul(SIMILARITY_MULTIPLIER))))
+        # print("   calculated : ", recommended.head(10))
+
+        grouped_df = recommended.groupby(['product_id', 'product_title'], as_index=False).mean()
+
+        print("   GROUPED : \n", grouped_df.head(10))
+
+    elif FORMULA == 4: 
+        recommended['weight-score'] = ( ((recommended['helpful_votes'].add(recommended['total_votes'])).mul(ALL_VOTES_MULTIPLIER)).add(1) )
+        recommended['rank-score'] = ( (recommended['weight-score']).mul(recommended['sentiment-score']))
+        recommended['rec-score'] = ( ((recommended['similarity_score'].mul(10)).add(recommended['rank-score'])).mul(SIMILARITY_MULTIPLIER))
+        # print("   calculated : ", recommended.head(10))
+
+        grouped_df = recommended.groupby(['product_id', 'product_title'], as_index=False).mean()
+
+        print("   GROUPED : \n", grouped_df.head(10))
+
+    # print(big_df.head())
+    # Sort DataFrame by calculated recommended score in descending order and show top 10 rows
+    top_10 = grouped_df.sort_values(by='rec-score', ascending=False)
+    top_10 = top_10.head(10)
+    
+    top_10_ids = top_10['product_id'].to_list()
+    try:
+        top_10_prices = get_price_from_list_IDs(top_10_ids)
+        top_10 = top_10.join(top_10_prices, on='product_id')
+    except:
+        print(top_10)
+        return top_10[['product_title','rec-score']],"Error in fetching product prices, showing result without price information"
+    print(top_10)
+    return top_10[['product_title','rec-score','price']],""
+
 if __name__ == "__main__":
-    generate_top_ten_v3()
+    generate_top_ten_with_prices()
